e88fd68e4aa7ecac7b3831373bea924ff1e5e6a2
work-ent <workentcartient@gmail.com>
2024-10-05 20:31:29 +0300

    Update config.js

--- dtbs/config.js
+++ dtbs/config.js
@@ -7,8 +7,8 @@
 */
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['254742491666']
-global.ownMain = '254742491666'
+global.owner = ['254796519826']
+global.ownMain = '254796519826'
 global.NamaOwner = '𝕶𝖎𝖓𝖌 𝕾𝖆𝖒' //
 global.sessionName = 'session'
 global.connect = true // 
